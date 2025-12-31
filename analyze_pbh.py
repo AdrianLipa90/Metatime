b826ac6ce582239ce10a42ae2938145cc845b217
Adrian Lipa <adrianlipa90@gmail.com>
2025-12-31 21:10:40 +0000

    Add license information to analyze_pbh.py

--- analyze_pbh.py
+++ analyze_pbh.py
@@ -1,3 +1,10 @@
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
